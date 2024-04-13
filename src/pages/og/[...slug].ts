6bc4605ed82560e39cc79e1ef1d38feb75c3f24d
nikita <contact@iamkorotaev.ru>
2024-04-13 19:32:22 +0500

    core: small fix OG

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -16,32 +16,28 @@ export const { getStaticPaths, GET } = OGImageRoute({
     return {
       title: page.data.title,
       description: page.data.description,
-      bgGradient: [[24, 24, 27]],
-      border: { color: [63, 63, 70], width: 20 },
-      padding: 120,
+      bgGradient: [
+      [255, 255, 255],
+      ],
+      border: { color: [56, 111, 228], width: 20, side: 'inline-start' },
+      padding: 60,
       font: {
       title: {
         size: 78,
+        color: [56, 111, 228],
         families: [
           'Work Sans',
           'Noto Sans Black',
-          'Noto Sans Arabic',
-          'Noto Sans SC Black',
-          'Noto Sans TC Black',
-          'Noto Sans JP Black',
         ],
         weight: 'ExtraBold',
       },
       description: {
+        color: [0, 0, 0],
         size: 45,
         lineHeight: 1.25,
         families: [
           'Work Sans',
           'Noto Sans',
-          'Noto Sans Arabic',
-          'Noto Sans SC',
-          'Noto Sans TC',
-          'Noto Sans JP',
         ],
         weight: 'Normal',
       },
@@ -52,18 +48,6 @@ export const { getStaticPaths, GET } = OGImageRoute({
 
       'https://api.fontsource.org/v1/fonts/noto-sans/cyrillic-400-normal.ttf',
       'https://api.fontsource.org/v1/fonts/noto-sans/cyrillic-900-normal.ttf',
-
-      'https://api.fontsource.org/v1/fonts/noto-sans-sc/chinese-simplified-400-normal.ttf',
-      'https://api.fontsource.org/v1/fonts/noto-sans-sc/chinese-simplified-900-normal.ttf',
-
-      'https://api.fontsource.org/v1/fonts/noto-sans-tc/chinese-traditional-400-normal.ttf',
-      'https://api.fontsource.org/v1/fonts/noto-sans-tc/chinese-traditional-900-normal.ttf',
-
-      'https://api.fontsource.org/v1/fonts/noto-sans-jp/japanese-400-normal.ttf',
-      'https://api.fontsource.org/v1/fonts/noto-sans-jp/japanese-900-normal.ttf',
-
-      'https://api.fontsource.org/v1/fonts/noto-sans-arabic/arabic-400-normal.ttf',
-      'https://api.fontsource.org/v1/fonts/noto-sans-arabic/arabic-800-normal.ttf',
     ],
     }
   },
