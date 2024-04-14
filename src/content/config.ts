e3b7849fb829ef71434bcfef53e70e3c3ba6b86e
nikita <contact@iamkorotaev.ru>
2024-04-14 21:33:31 +0500

    bvps rewrite

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from "astro:content";
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-  docs: defineCollection({
-    schema: docsSchema({
-      extend: z.object({
-        banner: z.object({ content: z.string() }).default({
-          content:
-            'Crypto payment Cloud Provider <a href="https://bitcoinvps.cloud">BitcoinVPS.cloud</a> 🌟',
-        }),
-      }),
-    }),
-  }),
+  docs: defineCollection({ schema: docsSchema() }),
 };
